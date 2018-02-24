657a196e5fca31ae546bed93a4934b98b6b8f69a
MS <MSRamesh@ES.AD.ADP.com>
2018-02-24 17:51:11 +0530

    Modularise About us Page

--- src/app/charity-app/about/about.module.ts
+++ src/app/charity-app/about/about.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AboutComponent } from './components/about.component';
+import { MissionComponent } from './components/mission/mission.component';
+import { TeamMemberComponent } from './components/team-member/team-member.component';
+import { DetailComponent } from './components/detail/detail.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
     declarations: [
-        AboutComponent
+        AboutComponent,
+        MissionComponent,
+        TeamMemberComponent,
+        DetailComponent,
+        HeaderComponent
     ],
     imports: [
         CommonModule
